3c9e3ebbf27c6223f1d26777ec4d03cff94d3909
Felipe Vargas <felipeng.eletrica@gmail.com>
2023-04-13 20:28:38 -0300

    Add presentations

--- Server-TCP-Multicliente.py
+++ Server-TCP-Multicliente.py
@@ -27,4 +27,5 @@ if __name__ == "__main__":
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
+    server.timeout = 10
     server.serve_forever()
